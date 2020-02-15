cd87074326fa36944b265848428ffff4f25136b0
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-15 10:16:01 +0000

    Added http request to reddit api

--- src/reddit_analyser/core.clj
+++ src/reddit_analyser/core.clj
@@ -2,6 +2,9 @@
   (:require [clj-http.client :as client])
   (:gen-class))
 
-(def options {:headers {"User-agent" "bond-007"}})
+(def options {:headers {"User-agent" "bond-007"}
+              :query-params {:limit 100}})
+
 (def url "https://www.reddit.com/r/Clojure.json")
+
 (client/get url options)
